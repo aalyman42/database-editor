5c0592f410615e02054d06331506876b1dfcceb7
aalyman42 <aalyman42@gmail.com>
2024-01-06 21:48:12 -0700

    allowed inquirer menu to return after user selections. added employee update function

--- index.js
+++ index.js
@@ -14,124 +14,266 @@ const queries = require("./db/queries.js");
 //     console.log(err);
 //   });
 
-inquirer
-  .prompt([
-    {
-      type: "list",
-      message: `WELCOME TO THE EMPLOYEE DATABASE            
-         
-         What would you like to do?`,
-      choices: [
-        "View Departments",
-        "View Roles",
-        "View Employees",
-        "Add a department",
-        "Add a role",
-        "Add an employee",
-        "Update an employee",
-      ],
-      name: "choice",
-    },
-  ])
-  .then(function (selection) {
-    const { choice } = selection;
-    console.log(choice);
-    if (choice == "View Departments") {
-      queries
-        .findAllDepartments()
-        .then((data) => {
-          const dataField = data;
-          const arrayData = dataField[0];
-          console.log(`
-id   |    name             
-------------------------------`);
-          for (i = 0; i < arrayData.length; i++) {
-            console.log(`${arrayData[i].id}    |   ${arrayData[i].name}  `);
-          }
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    }
-    if (choice == "View Roles") {
-      queries
-        .findAllRoles()
-        .then((data) => {
-          const dataField = data;
-          const arrayData = dataField[0];
-          console.log(`
-id   |    title          |    salary       |  department ID          
--------------------------------------------------------------`);
-          for (i = 0; i < arrayData.length; i++) {
-            console.log(
-              `${arrayData[i].id}    |   ${arrayData[i].title}       | ${arrayData[i].salary}       |${arrayData[i].department_id} `
-            );
-          }
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    }
-    if (choice == "View Employees") {
-      queries
-        .findAllEmployees()
-        .then((data) => {
-          const dataField = data;
-          const arrayData = dataField[0];
-          console.log(arrayData);
-          console.log(`
+const mainMenu = function (message) {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: `${message}`,
+        choices: [
+          "View Departments",
+          "View Roles",
+          "View Employees",
+          "Add a department",
+          "Add a role",
+          "Add an employee",
+          "Update an employee",
+          "Quit",
+        ],
+        name: "choice",
+      },
+    ])
+    .then(function (selection) {
+      const { choice } = selection;
+      if (choice == "View Departments") {
+        queries
+          .findAllDepartments()
+          .then((data) => {
+            const dataField = data;
+            const arrayData = dataField[0];
+            console.log(`
+id   name             
+---- ---------------`);
+            for (i = 0; i < arrayData.length; i++) {
+              console.log(`${arrayData[i].id}    ${arrayData[i].name}  `);
+            }
+            console.log(`
+            
+            `);
+          })
+          .then(() => {
+            return mainMenu(`anything else?`);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
+      if (choice == "View Roles") {
+        queries
+          .findAllRoles()
+          .then((data) => {
+            const dataField = data;
+            const arrayData = dataField[0];
+            console.log(`
+id   title                salary   department ID          
+---- -------------------- -------- -------------`);
+            for (i = 0; i < arrayData.length; i++) {
+              console.log(
+                `${arrayData[i].id}    ${arrayData[i].title}                ${arrayData[i].salary}   ${arrayData[i].department_id} `
+              );
+            }
+            console.log(`
+            
+            `);
+          })
+          .then(() => {
+            return mainMenu(`anything else?`);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
+      if (choice == "View Employees") {
+        queries
+          .findAllEmployees()
+          .then((data) => {
+            const dataField = data;
+            const arrayData = dataField[0];
+            console.log(`
 id   |    first name   |    last name   |  role ID      | manager ID    
 ---------------------------------------------------------------------------------`);
-          for (i = 0; i < arrayData.length; i++) {
+            for (i = 0; i < arrayData.length; i++) {
+              console.log(
+                `${arrayData[i].id}    |   ${arrayData[i].first_name}       | ${arrayData[i].last_name}       |${arrayData[i].role_id}          |${arrayData[i].manager_id}`
+              );
+            }
+            console.log(`
+            
+            `);
+          })
+          .then(() => {
+            return mainMenu(`anything else?`);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
+      if (choice == "Add a department") {
+        inquirer
+          .prompt([
+            {
+              type: "input",
+              message: "Please name your new Department:",
+              name: "depNameInput",
+            },
+          ])
+          .then(function (field) {
+            const { depNameInput } = field;
+            queries.addDepartment(depNameInput);
             console.log(
-              `${arrayData[i].id}    |   ${arrayData[i].first_name}       | ${arrayData[i].last_name}       |${arrayData[i].role_id}          |${arrayData[i].manager_id}`
+              `Succesfully added ${depNameInput} to department database!`
             );
-          }
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    }
-    if (choice == "Add a department") {
-      inquirer
-        .prompt([
-          {
-            type: "input",
-            message: "Please name your new Department:",
-            name: "depNameInput",
-          },
-        ])
-        .then(function (field) {
-          const { depNameInput } = field;
-          queries.addDepartment(depNameInput);
-          console.log(
-            `Succesfully added ${depNameInput} to department database!`
-          );
-        });
-    }
-    if (choice == "Add a role") {
-      inquirer
-        .prompt([
-          {
-            type: "input",
-            message: "Please name your new Role:",
-            name: "roleNameInput",
-          },
-          {
-            type: "input",
-            message: "What is this Role's salary?",
-            name: "salaryInput",
-          },
-          {
-            type: "input",
-            message: "By ID, what department does this role belong to?",
-            name: "deparmentIdInput",
-          },
-        ])
-        .then(function (field) {
-          const { roleNameInput, salaryInput, deparmentIdInput } = field;
-          queries.addRole(roleNameInput, salaryInput, deparmentIdInput);
-          console.log(`Succesfully added ${roleNameInput} to Roles database!`);
-        });
-    }
-  });
+            console.log(`
+            
+            `);
+          })
+          .then(() => {
+            return mainMenu(`anything else?`);
+          });
+      }
+      if (choice == "Add a role") {
+        inquirer
+          .prompt([
+            {
+              type: "input",
+              message: "Please name your new Role:",
+              name: "roleNameInput",
+            },
+            {
+              type: "input",
+              message: "What is this Role's salary?",
+              name: "salaryInput",
+            },
+            {
+              type: "input",
+              message: "By ID, what department does this role belong to?",
+              name: "deparmentIdInput",
+            },
+          ])
+          .then(function (field) {
+            const { roleNameInput, salaryInput, deparmentIdInput } = field;
+            queries.addRole(roleNameInput, salaryInput, deparmentIdInput);
+            console.log(
+              `Succesfully added ${roleNameInput} to Roles database!`
+            );
+            console.log(`
+            
+            `);
+          })
+          .then(() => {
+            return mainMenu(`anything else?`);
+          });
+      }
+      if (choice == "Add an employee") {
+        inquirer
+          .prompt([
+            {
+              type: "input",
+              message: "Employee first name:",
+              name: "firstNameInput",
+            },
+            {
+              type: "input",
+              message: "Employee last name:",
+              name: "lastNameInput",
+            },
+            {
+              type: "input",
+              message: "By ID, what is this employee's role?",
+              name: "roleIdInput",
+            },
+            {
+              type: "input",
+              message:
+                "By ID, who is this employee's manager? (input null if n/a)",
+              name: "managerIdInput",
+            },
+          ])
+          .then(function (field) {
+            const {
+              firstNameInput,
+              lastNameInput,
+              roleIdInput,
+              managerIdInput,
+            } = field;
+            queries.addEmployee(
+              firstNameInput,
+              lastNameInput,
+              roleIdInput,
+              managerIdInput
+            );
+            console.log(
+              `Succesfully added ${firstNameInput} ${lastNameInput} to Employee database!`
+            );
+            console.log(`
+            
+            `);
+          })
+          .then(() => {
+            return mainMenu(`anything else?`);
+          });
+      }
+      if (choice == "Update an employee") {
+        inquirer
+          .prompt([
+            {
+              type: "input",
+              message: "Employee ID to update:",
+              name: "employeeID",
+            },
+            {
+              type: "input",
+              message: "Update employee first name:",
+              name: "firstNameInput",
+            },
+            {
+              type: "input",
+              message: "Update employee last name:",
+              name: "lastNameInput",
+            },
+            {
+              type: "input",
+              message: "Update employee role ID:",
+              name: "roleIdInput",
+            },
+            {
+              type: "input",
+              message: "Update manager by ID: (input null if n/a)",
+              name: "managerIdInput",
+            },
+          ])
+          .then(function (field) {
+            const {
+              employeeID,
+              firstNameInput,
+              lastNameInput,
+              roleIdInput,
+              managerIdInput,
+            } = field;
+            queries.updateEmployee(
+              employeeID,
+              firstNameInput,
+              lastNameInput,
+              roleIdInput,
+              managerIdInput
+            );
+            console.log(
+              `Succesfully updated ${firstNameInput} ${lastNameInput} in Employee database!`
+            );
+            console.log(`
+            
+            `);
+          })
+          .then(() => {
+            return mainMenu(`anything else?`);
+          });
+      }
+      if (choice == "Quit") {
+        return console.log("goodbye!");
+      }
+    });
+};
+
+mainMenu(`WELCOME TO THE EMPLOYEE DATABASE            
+         
+What would you like to do?`);
